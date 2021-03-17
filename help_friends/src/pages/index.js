67a00c177073ce601a81eb1f0c80cbf23888c05f
Jhony Cacuango <jhony.cacuango@epn.edu.ec>
2021-03-17 09:43:07 -0500

    Pantalla principal

--- help_friends/src/pages/index.js
+++ help_friends/src/pages/index.js
@@ -6,61 +6,76 @@ import Routes from "../constants/routes";
 import Link from 'next/link';
 import React from "react";
 import IconsFooter from "@/components/IconsFooter";
+import Crucel from './Crucel'
+
+
+//FOOTER
+import FacebookIcon from '@material-ui/icons/Facebook';
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+import InstagramIcon from '@material-ui/icons/Instagram';
+//import Button from '@material-ui/core/Button';
+
+import Email from './Email';
+import Card from './Card';
+
+
 
 export default function Home() {
     const {login, user} = useAuth();
     return (
-        <div className={styles.container}>
+        <div className={styles.container} >
             <Head>
                 <title>help friend's</title>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
             <main className={styles.main}>
-                <h1 className={styles.title}>
-                    Ayuda a un amigo
-                </h1>
-                <Link href={'/posts'}>pubicaciones</Link>
+
                 <Grid container item xs={12}
                       alignItems='center'
                       direction='column'
                       justify='space-between'
                 >
-                    <Grid container style={{minHeight: '50vh'}}>
-                        <Grid item xs={12} sm={6}>
-                            <h3>Documentation</h3>
-                            <p>Find in-depth information about Next.js features and API.</p>
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <h3>Documentation</h3>
-                            <p>Find in-depth information about Next.js features and API.</p>
-                        </Grid>
+                    <br></br>
+                    <Grid container style={{minHeight: '0vh'}}>
+                        <Crucel/>
                     </Grid>
-                    <Grid container style={{minHeight: '50vh'}}>
-                        <Grid item xs={12} sm={6}>
-                            <h3>Documentation</h3>
-                            <p>Find in-depth information about Next.js features and API.</p>
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <h3>Documentation</h3>
-                            <p>Find in-depth information about Next.js features and API.</p>
-                        </Grid>
+
+
+                    <Grid  style={{minHeight: '0vh'}} >
+                        <Card/>
                     </Grid>
-                    <Grid container style={{minHeight: '50vh'}}>
-                        <Grid item xs={12} sm={6}>
-                            <h3>Documentation</h3>
-                            <p>Find in-depth information about Next.js features and API.</p>
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <h3>Documentation</h3>
-                            <p>Find in-depth information about Next.js features and API.</p>
-                        </Grid>
+
+                    <Grid container style={{minHeight: '0vh'}}>
+                       <Email/>
                     </Grid>
+
                 </Grid>
 
             </main>
 
-            <footer>
-                <IconsFooter/>
+            <footer >
+                <Grid container spacing={4}>
+                    <Grid item xs>
+                        <div class="">
+
+                        </div>
+                    </Grid>
+                    <Grid item xs>
+
+                        <Button href="https://www.facebook.com/"  >  <FacebookIcon color="primary" /></Button>
+
+                    </Grid>
+                    <Grid item xs>
+
+                        <Button href="https://web.whatsapp.com/"  >   <WhatsAppIcon color="primary" /></Button>
+
+                    </Grid>
+                    <Grid item xs>
+
+                        <Button href="https://www.instagram.com/"  ><InstagramIcon color="primary" /></Button>
+
+                    </Grid>
+                </Grid>
             </footer>
         </div>
     )
